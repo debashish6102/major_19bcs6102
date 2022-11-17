a3130067276c7695ef69710bce96ac4b07f646d7
debashish6102 <54812335+debashish6102@users.noreply.github.com>
2022-11-17 21:32:07 +0530

    7th commit

--- static/script.js
+++ static/script.js
@@ -1,59 +1,15 @@
-//selecting all required elements
-const dropArea = document.querySelector(".drag-area"),
-dragText = dropArea.querySelector("header"),
-button = dropArea.querySelector("button"),
-input = dropArea.querySelector("input");
-let file; //this is a global variable and we'll use it inside multiple functions
+var input = document.getElementById( 'btn' );
+var infoArea = document.getElementById( 'content' );
+input.addEventListener( 'change', showFileName );
 
+function showFileName( event ) {
 
+  // the change event gives us the input it occurred in
+  var input = event.srcElement;
 
-button.onclick = ()=>{
-  input.click(); //if user click on the button then the input also clicked
-  return false;
+  // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
+  var fileName = input.files[0].name;
+
+  // use fileName however fits your app best, i.e. add it into a div
+  infoArea.textContent = fileName;
 }
-
-input.addEventListener("change", function(){
-  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-  file = this.files[0];
-  dropArea.classList.add("active");
-  showFile(); //calling function
-});
-//
-//
-////If user Drag File Over DropArea
-//dropArea.addEventListener("dragover", (event)=>{
-//  event.preventDefault(); //preventing from default behaviour
-//  dropArea.classList.add("active");
-//  dragText.textContent = "Release to Upload File";
-//});
-//
-////If user leave dragged File from DropArea
-//dropArea.addEventListener("dragleave", ()=>{
-//  dropArea.classList.remove("active");
-//  dragText.textContent = "Drag & Drop to Upload File";
-//});
-//
-////If user drop File on DropArea
-//dropArea.addEventListener("drop", (event)=>{
-//  event.preventDefault(); //preventing from default behaviour
-//  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
-//  file = event.dataTransfer.files[0];
-//  showFile(); //calling function
-//});
-
-    //function showFile(){
-      //let fileType = file.type; //getting selected file type
-      //let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-      //if(validExtensions.includes(fileType)){ //if user selected file is an image file
-        //let fileReader = new FileReader(); //creating new FileReader object
-       // fileReader.onload = ()=>{
-        //  let fileURL = fileReader.result; //passing user file source in fileURL variable
-        //    dropArea.innerHTML = `<img src="${fileURL}" alt="image">`; //adding that created img tag inside dropArea container
-      //  }
-        //fileReader.readAsDataURL(file);
-      //}else{
-      //  alert("This is not an Image File!");
-      //  dropArea.classList.remove("active");
-      //  dragText.textContent = "Drag & Drop to Upload File";
-     // }
-//}
